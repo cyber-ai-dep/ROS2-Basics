c4c181a8e0b9afbf49014bb28114f4e359330e95
Mohammad <mohammadcyberai@gmail.com>
2026-02-28 12:25:10 +0300

    Add robot description package and TF visualization example

---
+++ workshops/visualization/urdf-rviz/code/examples/my_robot_description/launch/display.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('my_robot_description')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'my_robot_arm.urdf')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_file).read()}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
